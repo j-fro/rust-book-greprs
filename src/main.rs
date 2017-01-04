3fac6b2c03fe8dc8b771f7094b44d0a36f89cc16
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-03 21:18:40 -0600

    read from supplied file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    let args: Vec<String> = env::args().collect::
+    let args: Vec<String> = env::args().collect();
+
+    let search = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", search);
+    println!("Searching in {}", filename);
+
+    let mut f = File::open(filename).expect("File not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("Something went wrong when reading the file");
+
+    println!("With text {}", contents);
 }
