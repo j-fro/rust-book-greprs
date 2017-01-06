bdc021fe704a39a0c616bbf50cbf94545d449b40
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-06 13:13:38 -0600

    refactored to lib and added tests

---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+
+pub struct Config {
+    pub search: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let search = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {
+            search: search,
+            filename: filename,
+        })
+    }
+}
+
+fn grep<'a>(search: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(search) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    for line in grep(&config.search, &contents) {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use grep;
+
+    #[test]
+    fn case_sensitive() {
+        let search = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], grep(search, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let search = "rust";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], grep(search, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
+extern crate greprs;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+
+use greprs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let search = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", search);
-    println!("Searching in {}", filename);
-
-    let mut f = File::open(filename).expect("File not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("Something went wrong when reading the file");
-
-    println!("With text {}", contents);
+    if let Err(e) = greprs::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
