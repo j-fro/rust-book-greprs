4c72ce313ab439f8f0f70bdafaf510efca340091
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-06 13:33:17 -0600

    updated with env variables and write to stderr

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
@@ -5,6 +6,7 @@ use std::error::Error;
 pub struct Config {
     pub search: String,
     pub filename: String,
+    pub case_sensitive: bool
 }
 
 impl Config {
@@ -13,12 +15,21 @@ impl Config {
             return Err("not enough arguments");
         }
 
+        let mut case_sensitive = true;
+
+        for (name, _) in env::vars() {
+            if name == "CASE_INSENSITIVE" {
+                case_sensitive = false;
+            }
+        }
+
         let search = args[1].clone();
         let filename = args[2].clone();
 
         Ok(Config {
             search: search,
             filename: filename,
+            case_sensitive: case_sensitive
         })
     }
 }
@@ -34,13 +45,31 @@ fn grep<'a>(search: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+fn grep_case_insensitive<'a>(search: &str, contents: &'a str) -> Vec<&'a str> {
+    let search = search.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&search) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    for line in grep(&config.search, &contents) {
+    let results = if config.case_sensitive {
+        grep(&config.search, &contents)
+    } else {
+        grep_case_insensitive(&config.search, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -50,6 +79,7 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 #[cfg(test)]
 mod test {
     use grep;
+    use grep_case_insensitive;
 
     #[test]
     fn case_sensitive() {
@@ -72,6 +102,6 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
-        assert_eq!(vec!["Rust:", "Trust me."], grep(search, contents));
+        assert_eq!(vec!["Rust:", "Trust me."], grep_case_insensitive(search, contents));
     }
 }
